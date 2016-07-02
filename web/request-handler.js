0f20453bc8c57162770bd523dc3de5071cb1e61d
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-02 19:31:43 +0400

    Check directory and return requested archived file

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,9 +6,26 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-	fs.readFile(path.join(archive.paths.siteAssets, 'index.html'),'utf8', function(err,data){
-		res.writeHead(200, {"Content-Type": "text/html"})
-		res.write(data);
-		res.end();
-	});
+	var url = req.url;
+	if (url === '/'){ 
+		fs.readFile(path.join(archive.paths.siteAssets, 'index.html'),'utf8', function(err,data){
+			res.writeHead(200, {"Content-Type": "text/html"})
+			res.write(data);
+			res.end();
+		});		
+	} else{
+		fs.exists(path.join(archive.paths.archivedSites, url), (exists) => {
+			if(exists){
+				fs.readFile(path.join(archive.paths.archivedSites, url),'utf8', function(err,data){
+					res.writeHead(200, {"Content-Type": "text/html"})
+					res.write(data);
+					res.end();
+				});	
+			} else {
+				res.writeHead(404, {});				
+				res.end('404 not found');
+			}
+		})
+	}
+
 };
