f82f0e6da4b51c6cb9eb9c03dc2eda7312691ef5
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-02 17:36:36 +0400

    Serve index.html statically

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,14 @@
 var path = require('path');
+var fs = require('fs');
+var httpHelper = require('path');
+
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+	fs.readFile(path.join(archive.paths.siteAssets, 'index.html'),'utf8', function(err,data){
+		res.writeHead(200, {"Content-Type": "text/html"})
+		res.write(data);
+		res.end();
+	});
 };
