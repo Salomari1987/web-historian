a69b50a96dc73b09d7c78c343118869faa4331e1
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-03 19:47:15 +0400

    Handle POST requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var fs = require('fs');
-var httpHelper = require('path');
+var path = require('path');
 var querystring = require('querystring')
 var archive = require('../helpers/archive-helpers');
+var httpHelper = require('./http-helpers')
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
@@ -15,39 +16,16 @@ exports.handleRequest = function (req, res) {
 		})
 		req.on('end', function(){
 			var decodedBody = querystring.parse(fullBody);
-			var textTowrite = decodedBody.url+"\n"
-			fs.open(archive.paths.list, 'a', 666, function (e, id){
-				fs.write(id, textTowrite, null, 'utf8', function(){
-					fs.close(id, function(){
-						console.log(fs.readFileSync(archive.paths.list, 'utf8'), 'aaaaaaa')
-						console.log('teeeeet')
-					})
-				})
-				
-			})
+			archive.addUrlToList(decodedBody.url, function(){
+				res.writeHead(302, {})
+				res.end()
+			})					
 		})
 		
-	} else if (url === '/'){
-		console.log(4) 
-		fs.readFile(path.join(archive.paths.siteAssets, 'index.html'),'utf8', function(err,data){
-			res.writeHead(200, {"Content-Type": "text/html"})
-			res.write(data);
-			res.end();
-		});		
-	} else{
-		console.log(5)
-		fs.exists(path.join(archive.paths.archivedSites, url), (exists) => {
-			if(exists){
-				fs.readFile(path.join(archive.paths.archivedSites, url),'utf8', function(err,data){
-					res.writeHead(200, {"Content-Type": "text/html"})
-					res.write(data);
-					res.end();
-				});	
-			} else {
-				res.writeHead(404, {});				
-				res.end('404 not found');
-			}
+	} else {
+		httpHelper.serveAssets(null, url, function(resCode, d){
+			res.writeHead(resCode);
+			res.end(d);
 		})
 	}
-
 };
